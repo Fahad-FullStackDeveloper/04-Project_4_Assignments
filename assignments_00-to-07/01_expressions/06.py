c532addc1d9306e1c8be75f8dd778bc5a075a5f3
Fahad-FullStackDeveloper <fahad.dude2010@gmail.com>
2025-04-12 14:23:15 +0500

    completed Assignment 4, Project01, task01, 06.py

---
+++ assignments_00-to-07/01_expressions/06.py
@@ -0,0 +1,23 @@
+"""
+Simulate rolling two dice, and prints results of each
+roll as well as the total.
+"""
+# Import the random library which lets us simulate random things like dice!
+import random
+
+# Number of sides on each die to roll
+NUM_SIDES: int = 6
+
+def main():
+    die1 : int = random.randint(1, NUM_SIDES)
+    die2 : int = random.randint(1, NUM_SIDES)
+    total : int = die1 + die2
+    
+    print("\nDice have", NUM_SIDES, "sides each.")
+    print("\tRolled First die:" , die1)
+    print("\tRolled Second die:", die2)
+    # Print the total of the two dice
+    print("\tTotal of two dice:" , total)
+
+if __name__ == "__main__":
+    main() 
